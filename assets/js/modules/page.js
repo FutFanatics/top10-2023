2c6a4bbb7be81a7feb8e4b7c2f841fdb4a0650c4
Manoela Moreno Frattini <manoelamoreno78@gmail.com>
2023-10-11 17:43:57 -0300

    vitrines

--- assets/js/modules/page.js
+++ assets/js/modules/page.js
@@ -2,7 +2,7 @@
 
 	var containerinstruction= $('.instruction__slick')
 	var containerstep= $('.slick__step')
-
+	var containertop= $('.slick__top')
 
 	containerinstruction.slick({
 		autoplay: false,
@@ -24,6 +24,7 @@
 			
 			}]
 	});
+	
 	containerstep.slick({
 		autoplay: false,
 		autoplaySpeed: 4000,
@@ -45,6 +46,26 @@
 			}]
 	});
 
+	containertop.slick({
+		autoplay: false,
+		autoplaySpeed: 4000,
+		pauseOnFocus: false,
+		pauseOnHover: false,
+		arrows: false,
+		dots: false,
+		adaptiveHeight: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		responsive: [{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+
+						}
+			
+			}]
+	});
 
 	function copyToClipboard(element) {
 		var $temp = $("<input>");
@@ -62,6 +83,250 @@
 			$($el).removeClass('copied');
 		}, 3000);
 	});
+
+	$('.selection').on('click', function(){
+		$('.selection').addClass('active');
+		$('.selection_shirt').addClass('active');
+		$('.club').removeClass('active');
+		$('.club_shirt').removeClass('active');
+		$('.regional').removeClass('active');
+		$('.regional_shirt').removeClass('active');
+	})
+
+	$('.club').on('click', function(){
+		$('.selection').removeClass('active');
+		$('.selection_shirt').removeClass('active');
+		$('.club').addClass('active');
+		$('.club_shirt').addClass('active');
+		$('.regional').removeClass('active');
+		$('.regional_shirt').removeClass('active');
+	})
+	$('.regional').on('click', function(){
+		$('.selection').removeClass('active');
+		$('.selection_shirt').removeClass('active');
+		$('.club').removeClass('active');
+		$('.club_shirt').removeClass('active');
+		$('.regional').addClass('active');
+		$('.regional_shirt').addClass('active');
+	})
+
+	$('.see-more_selection').on('click', function() {
+		console.log($(this).siblings('.selection_shirt').hasClass('all'));
+
+		if (!$(this).siblings('.selection_shirt').hasClass('all')) {
+			$(this).html('Mostrar menos -');
+			$(this).siblings('.selection_shirt').addClass('all');
+		} else { 
+			$(this).html('Ver tudo');
+			$(this).siblings('.selection_shirt').removeClass('all');
+		}
+		return false;
+	});
+
+	$('.see-more_selection').on('click', function() {
+		console.log($(this).siblings('.selection_shirt-content').hasClass('all'));
+
+		if (!$(this).siblings('.selection_shirt-content').hasClass('all')) {
+			$(this).html('Ver menos');
+			$(this).siblings('.selection_shirt-content').addClass('all');
+		} else { 
+			$(this).html('Ver tudo');
+			$(this).siblings('.selection_shirt-content').removeClass('all');
+		}
+		return false;
+	});
+
+	$('.see-more_club').on('click', function() {
+		console.log($(this).siblings('.club_shirt-content').hasClass('all'));
+
+		if (!$(this).siblings('.club_shirt-content').hasClass('all')) {
+			$(this).html('Ver menos');
+			$(this).siblings('.club_shirt-content').addClass('all');
+		} else { 
+			$(this).html('Ver tudo');
+			$(this).siblings('.club_shirt-content').removeClass('all');
+		}
+		return false;
+	});
+	$('.see-more_regional').on('click', function() {
+		console.log($(this).siblings('.regional_shirt').hasClass('all'));               
+
+		if (!$(this).siblings('.regional_shirt').hasClass('all')) {
+			$(this).html('Ver tudo');
+			$(this).siblings('.regional_shirt').addClass('all');
+		} else { 
+			$(this).html('Ver tudo');
+			$(this).siblings('.regional_shirt').removeClass('all');
+		}
+		return false;
+	});
+
+	var produto = [120960,114348,114386,117476,114394,116495];
+
+    var template_produtos =
+		'<div class="item">' +
+			'<a class="link">' +
+				'<div class="foto"><span class="discount d-none"></span>' +
+					'<span class="thumb">' +
+						'<img class="lozad img-fluid" src="assets/img/pixel-2.png">' +
+					'</span>' +
+				'</div>' +
+				'<div class="tags"></div>' +
+				'<h2 class="title"></h2>' +
+				'<div class="price"></div>' +
+				'<div class="product-variants">' +
+					'<div class="variants-slider"></div>' +
+				'</div>' +
+				'<div class="product-actions"></div>' +
+			'</a>' +
+		'</div>';
+
+    var vitrine = $('#vitrine-fut');
+
+	
+	$.getJSON('https://www.futfanatics.com.br/web_api/products?id=' + produto.join(","), '', function (data) {
+		if (data) {
+			var variants = [];
+
+			data.Products.forEach(function(dataProduct){
+
+				var product = dataProduct.Product
+
+				if (product.available != 0) {
+					var template = jQuery(template_produtos);
+
+					var link = product.url.https;
+					var img = product.ProductImage[0].thumbs[180].https;
+					var title = product.name;
+					var pricePromo = product.promotional_price;
+					var price = product.price;
+					var percentDiscount = 100 - (pricePromo/price) * 100;
+					var payment = product.payment_option;
+					var personalization = product.Properties['Permite Personalização'] == 'Sim' ? true : false;
+					var release = product.release == "1" ? true : false;
+
+					template.find('.link').attr('href', link);
+					template.find('.foto span img').attr('src', img);
+					template.find('.title').html(title);
+
+					if (percentDiscount < 100) {
+						template.find('.foto .discount').html('▾ ' + percentDiscount.toFixed() + '%').removeClass('d-none');
+					}
+
+					// lozad('.lozad', {
+					//     load: function(target)
+ 
+					//             target.src = target.dataset.src;
+					//             target.onload = function() {
+					//                 target.classList.add('fadein');
+					//             }
+					//     }
+					// }).observe();
+
+					if (release && pricePromo != 0 && personalization) {
+						template.find('.tags').html('<span class="lancamento">Lançamento</span> <span class="oferta">Oferta</span>');
+					} else if (pricePromo != 0 && personalization) {
+						template.find('.tags').html('<span class="oferta">Oferta</span> <span class="personalize">Personalize</span>');
+					} else if (product.release && pricePromo != 0) {
+						template.find('.tags').html('<span class="lancamento">Lançamento</span><span class="oferta">Oferta</span>');
+					} else {
+
+						if (release) {
+							template.find('.tags').html('<span class="lancamento">Lançamento</span>');
+						} else if (pricePromo != 0) {
+							template.find('.tags').html('<span class="oferta">Oferta</span>');
+						} else if (personalization) {
+							template.find('.tags').html('<span class="personalize">Personalize</span>');
+						}
+					}
+
+					if (pricePromo != 0) {
+						template.find('.price').html('<div class="old-price">R$ ' + price.replace('.', ',') + '</div><div class="current-price">R$ ' + pricePromo.replace('.', ',') + '</div>');
+					} else {
+						template.find('.price').html('<div class="current-price">R$ ' + price.replace('.', ',') + '</div>');
+					}
+				
+
+					
+					// variants[0].forEach(function(row){
+					//     if (row) {
+					//         template.find('.variants-slider').append(
+					//             '<div class="variants-item">' +
+					//                 '<button type="button" data-variant="'+ row.Variant.id +'">' + 
+					//                     row.Variant.Sku[0].value + 
+					//                 '</button>' +
+					//             '</div>'
+					//         );
+					//     }
+					// });
+
+					// template.find('.product-actions').html(
+					//     '<a href="#" class="bt_comprar d-flex justify-content-center" title="Adicionar este item ao seu carrinho">' +
+					//         '<i></i>' +
+					//         '<span>Comprar</span>' +
+					//     '</a>'
+					// );
+
+	//                vitrine.append(template);
+					vitrine.slick('slickAdd', template);
+				}
+			});
+
+			// jQuery.ajax({
+			//     url: "https://www.futfanatics.com.br/web_api/variants?product_id=" + product.id,
+			//     //context: document.body,
+			//     async: false,
+			//     method: "GET",
+			//     crossDomain: true,
+			//   }).done(function(data) {
+			//       console.log("Data:",data);
+			//     variants.push(data.Variants);
+			//   });
+				
+			
+		}
+	});
+
+	vitrine.html('');
+
+
+	if (isMobile()) {
+		vitrine.slick({
+			autoplay: false,
+			infinite: true,
+			speed: 500,
+			arrows: false,
+			dots: true,
+			slidesToShow: 2,
+			slidesToScroll: 2,
+	//        lazyLoad: 'ondemand',
+			prevArrow: $('.slick-nav_vitrine').find('.slick-prev'),
+			nextArrow: $('.slick-nav_vitrine').find('.slick-next'),
+		});
+
+	} else {
+
+		vitrine.slick({
+			autoplay: false,
+			infinite: true,
+			speed: 500,
+			arrows: true,
+			dots: true,
+			slidesToShow: 5,
+			slidesToScroll: 2,
+	//        lazyLoad: 'ondemand',
+			prevArrow: $('.slick-nav_vitrine').find('.slick-prev'),
+			nextArrow: $('.slick-nav_vitrine').find('.slick-next'),
+		});
+
+	}
+
+	function isMobile() {
+		if (window.innerWidth > 991) {
+			return false;
+		}
+		return true;
+	}
 
 	$('.c-modalVote form').on('submit', function(event) {
 		console.log('clickou');
